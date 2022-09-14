1226dc6af7888d7ee4f27b50e7c1b291eab5032a
David Richardson <drichardson42@gatech.edu>
2022-09-14 13:58:42 -0400

    Get Versioneer working

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+import versioneer
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+with open("requirements.txt", "r") as f:
+    requirements = [line.strip() for line in f]
+
+setuptools.setup(
+    name="pyday-night",
+    description="Tools for working with the Day-Night Terminator",
+    long_description=long_description,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="David Richardson",
+    author_email="drichardson42@gatech.edu",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(include=["pydaynight"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+    ],
+)
